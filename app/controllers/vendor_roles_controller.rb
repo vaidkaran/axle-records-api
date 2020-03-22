66bfbda3ac6a57ac887dd653ad980f6252cda42d
Karan Vaid <vaidkaran89@gmail.com>
2020-03-22 13:01:28 +0530

    vendor_roles crud

--- app/controllers/vendor_roles_controller.rb
+++ app/controllers/vendor_roles_controller.rb
@@ -1,2 +1,28 @@
 class VendorRolesController < ApplicationController
+  before_action :ensure_site_admin, except: [:index]
+
+  def index
+    @vendor_roles = VendorRole.all
+    render json: @vendor_roles, status: :ok
+  end
+
+  def create
+    @vendor_role = VendorRole.create! vendor_role_params
+    render json: @vendor_role, status: :created
+  end
+
+  def update
+    @vendor_role = VendorRole.find params[:id]
+    if @vendor_role.update_attributes(vendor_role_params)
+      render json: @vendor_role, status: :ok
+    else
+      render json: {error: 'Something went wrong. Could not update vendor role'}, status: :internal_server_error
+    end
+  end
+
+  private
+  def vendor_role_params
+    params.permit(:name)
+  end
+
 end
