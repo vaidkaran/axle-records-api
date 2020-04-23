a2c398911482ffef6913eec8eaa725ecbc81da92
Karan Vaid <vaidkaran89@gmail.com>
2020-04-23 15:03:26 +0530

    removed table shop_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_070833) do
+ActiveRecord::Schema.define(version: 2020_04_23_093007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
