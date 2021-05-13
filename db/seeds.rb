f647edfd65e2e34e658ef416de6345ebaf9e5595
Karan Vaid <vaidkaran89@gmail.com>
2021-05-14 02:21:31 +0530

    added db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,28 @@
 
 require('securerandom')
 require_relative('./seed_helper')
+include SeedHelper
 
 # site_roles
-admin_role = SiteRole.create(name: 'admin')
-customer_role = SiteRole.create(name: 'customer')
-vendor_role = SiteRole.create(name: 'vendor')
+admin_role = SiteRole.create!(name: 'admin')
+customer_role = SiteRole.create!(name: 'customer')
+vendor_role = SiteRole.create!(name: 'vendor')
 #----------------------------------------------------------------------------------------
 
 
 # admin users
-admin1 = User.create(
+admin1 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Admin User',
-  email: 'admin@ar.com',
+  email: 'admin1@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
-admin2 = User.create(
+admin2 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Admin User',
-  email: 'admin@ar.com',
+  email: 'admin2@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
 admin1.site_roles << admin_role
@@ -36,18 +37,18 @@ admin2.site_roles << admin_role
 #----------------------------------------------------------------------------------------
 
 # customer users
-customer1 = User.create(
+customer1 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Customer User',
-  email: 'customer@ar.com',
+  email: 'customer1@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
-customer2 = User.create(
+customer2 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Customer User',
-  email: 'customer@ar.com',
+  email: 'customer2@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
 customer1.site_roles << customer_role
@@ -55,18 +56,18 @@ customer2.site_roles << customer_role
 #----------------------------------------------------------------------------------------
 
 # vendor users
-vendor1 = User.create(
+vendor1 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Vendor User',
-  email: 'vendor@ar.com',
+  email: 'vendor1@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
-vendor2 = User.create(
+vendor2 = User.create!(
   provider: 'https://securetoken.google.com/axle-records-firebase',
   uid: SecureRandom.hex,
   name: 'Vendor User',
-  email: 'vendor@ar.com',
+  email: 'vendor2@ar.com',
   picture: 'https://lh6.googleusercontent.com/-iYm0d6moMeM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmRJ4rPrA_8HbgkgcFrM81Ltetdpg/s96-c/photo.jpg'
 )
 vendor1.site_roles << vendor_role
@@ -74,9 +75,9 @@ vendor2.site_roles << vendor_role
 #----------------------------------------------------------------------------------------
 
 # vehicle categories
-car_category = VehicleCategory.create(name: 'car')
-bike_category = VehicleCategory.create(name: 'bike')
-scooter_category = VehicleCategory.create(name: 'scooter')
+car_category = VehicleCategory.create!(name: 'car')
+bike_category = VehicleCategory.create!(name: 'bike')
+scooter_category = VehicleCategory.create!(name: 'scooter')
 
 #----------------------------------------------------------------------------------------
 
@@ -85,16 +86,16 @@ def create_vehicle_data(vehicle_data, vehicle_category)
   vehicle_data.each do |vehicle|
     brand_name = vehicle[:brand]
     # create vehicle_brand
-    vehicle_brand = VehicleBrand.create(name: brand_name)
+    vehicle_brand = VehicleBrand.create!(name: brand_name)
 
-    car[:models].each do |model|
+    vehicle[:models].each do |model|
       model_name = model[:model]
       # create vehicle_model
-      vehicle_model = vehicle_brand.vehicle_models.create(name: model_name, vehicle_category_id: vehicle_category.id)
+      vehicle_model = vehicle_brand.vehicle_models.create!(name: model_name, vehicle_category_id: vehicle_category.id)
 
       model[:variants].each do |variant_name|
         # create vehicle_variant
-        vehicle_model.vehicle_variants.create(name: variant_name)
+        vehicle_model.vehicle_variants.create!(name: variant_name)
       end
 
     end
@@ -107,5 +108,34 @@ create_vehicle_data(bikes, bike_category)
 create_vehicle_data(scooters, scooter_category)
 
 
-
 # vehicles
+vehicle_model_length = VehicleModel.all.length
+vehicle_model1 = VehicleModel.all[rand(vehicle_model_length)]
+customer1.vehicles.create!({
+  name: vehicle_model1.name,
+  vehicle_brand_id: vehicle_model1.vehicle_brand.id,
+  vehicle_model_id: vehicle_model1.id,
+  vehicle_variant_id: vehicle_model1.vehicle_variants.first.id,
+})
+vehicle_model2 = VehicleModel.all[rand(vehicle_model_length)]
+customer1.vehicles.create!({
+  name: vehicle_model2.name,
+  vehicle_brand_id: vehicle_model2.vehicle_brand.id,
+  vehicle_model_id: vehicle_model2.id,
+  vehicle_variant_id: vehicle_model2.vehicle_variants.first.id,
+})
+vehicle_model3 = VehicleModel.all[rand(vehicle_model_length)]
+customer2.vehicles.create!({
+  name: vehicle_model3.name,
+  vehicle_brand_id: vehicle_model3.vehicle_brand.id,
+  vehicle_model_id: vehicle_model3.id,
+  vehicle_variant_id: vehicle_model3.vehicle_variants.first.id,
+})
+vehicle_model4 = VehicleModel.all[rand(vehicle_model_length)]
+customer2.vehicles.create!({
+  name: vehicle_model4.name,
+  vehicle_brand_id: vehicle_model4.vehicle_brand.id,
+  vehicle_model_id: vehicle_model4.id,
+  vehicle_variant_id: vehicle_model4.vehicle_variants.first.id,
+})
+
