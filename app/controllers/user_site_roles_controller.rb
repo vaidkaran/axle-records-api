85220c15025fc42d10a5c3c8088c7176d1f0fb5f
Karan Vaid <vaidkaran89@gmail.com>
2021-05-16 19:58:08 +0530

    removed @current_user and used current_user and set_current_user for setting and getting

--- app/controllers/user_site_roles_controller.rb
+++ app/controllers/user_site_roles_controller.rb
@@ -3,14 +3,20 @@ class UserSiteRolesController < ApplicationController
     site_role = SiteRole.find(params[:site_role_id])
 
     if (!params[:user_id].nil?)
+      # presence of params[:user_id] means you're adding a site_role to another user
       if (current_user_is_admin)
         user = User.find(params[:user_id])
       else
-        render json: {message: "You need to be a site admin to add a site role for someone"}, status: :ok
+        render json: {message: "You need to be a site admin to add a site role for someone"}, status: :unauthorized
+        return
       end
+    elsif (site_role.name == 'admin')
+      # no params[:user_id] means you're adding your own site_role
+      render json: {message: "Cannot make yourself an admin"}, status: :unauthorized
+      return
     else
       # no params[:user_id] means you're adding your own site_role
-      user = @current_user
+      user = current_user
     end
     user.site_roles << site_role
     render json: {message: "User added as #{site_role.name}"}, status: :created
@@ -18,12 +24,12 @@ class UserSiteRolesController < ApplicationController
 
   def delete_role
     site_role = SiteRole.find(params[:site_role_id])
-    @current_user.site_roles.find(site_role.id).delete
+    current_user.site_roles.find(site_role.id).delete
     render json: {message: "User is no longer #{site_role.name}"}, status: :ok
   end
 
   def show_roles
-    user = @current_user
+    user = current_user
     if (params[:user_id] && current_user_is_admin)
       user = User.find(params[:user_id])
     end
@@ -32,6 +38,6 @@ class UserSiteRolesController < ApplicationController
 
   private
   def current_user_is_admin
-    return @current_user.site_roles.include? SiteRole.find_by(name: :admin)
+    return current_user.site_roles.include? SiteRole.find_by(name: :admin)
   end
 end
