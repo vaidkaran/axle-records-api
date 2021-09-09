8d1449205f32d280146dafc945b138cb046a946e
Karan Vaid <vaidkaran89@gmail.com>
2021-09-09 11:41:01 +0530

    added debug to test.rb env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  ########### copied over from production.rb to have more info in logs#########
+  config.log_level = :debug
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
