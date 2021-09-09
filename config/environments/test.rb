75614e4b6d566de16445ad6710e8a5f6a6576772
Karan Vaid <vaidkaran89@gmail.com>
2021-09-09 11:50:40 +0530

    tried another log statement in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,6 +49,11 @@ Rails.application.configure do
 
   ########### copied over from production.rb to have more info in logs#########
   config.log_level = :debug
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
