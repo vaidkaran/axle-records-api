b27425797f661029841fb69daeb51e6dab243d75
Karan Vaid <vaidkaran89@gmail.com>
2021-10-04 13:36:06 +0530

    dont need to provide shop_id when updating job_profiles

--- app/controllers/job_profiles_controller.rb
+++ app/controllers/job_profiles_controller.rb
@@ -1,11 +1,10 @@
 class JobProfilesController < ApplicationController
-  before_action -> {ensure_shop_admin params[:shop_id]}, only: [:create, :update]
-  before_action -> {ensure_shop_admin get_shop_id_in_destroy}, only: [:destroy]
+  before_action -> {ensure_shop_admin get_shop_id_from_params}, only: [:create]
+  before_action -> {ensure_shop_admin get_shop_id_from_id}, only: [:destroy, :update]
 
   def index
     @job_profiles = JobProfile.where(job_profile_search_params)
-    # render json: @job_profiles
-    render json: @job_profiles.to_json({only: [:id, :name, :job_id, :shop_id],
+    render json: @job_profiles.to_json({only: [:id, :name, :job_id, :shop_id, :price],
       include: [{job: {only: [:id, :name, :description, :user_defined]}}]})
   end
 
@@ -49,7 +48,13 @@ class JobProfilesController < ApplicationController
     params.permit(:price)
   end
 
-  def get_shop_id_in_destroy
+  def get_shop_id_from_params
+    unless params[:shop_id]
+      render json: {error: 'shop_id not found'}, status: :bad_request
+    end
+  end
+
+  def get_shop_id_from_id
     @job_profile = JobProfile.find params[:id]
     return @job_profile.shop_id
   end
